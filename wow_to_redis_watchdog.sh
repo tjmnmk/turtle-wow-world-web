b0689bf2eabf2517fb7fcceeafdd0470ad0d205f
Adam Bambuch <adam.bambuch2@gmail.com>
2024-07-12 03:01:15 +0200

    Update wow_to_redis_watchdog.sh

--- wow_to_redis_watchdog.sh
+++ wow_to_redis_watchdog.sh
@@ -1,14 +1,15 @@
 #! /bin/bash -u
 
+MIN_TTL=6300 # 7200 - 15 * 60
+
 # get max_id key from redis
 max_id=$(redis-cli get max_id)
 
-
 # get message by max_id
-message=$(redis-cli keys "$max_id")
-
-
-if echo "$message" |grep -q "(empty list or set)"; then
-    systemctl restart wow-to-redis
+ttl=$(redis-cli ttl "$max_id" |awk '{print $1}')
+if [ ! -z "$ttl" ] && [ "$ttl" -gt "$MIN_TTL" ]; then
+    exit 0
 fi
+
+systemctl restart tclib_to_redis
 
